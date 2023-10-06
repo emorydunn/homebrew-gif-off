68167bd3e1a975808eaf843c411c597664ad9387
Emory Dunn <emory@emorydunn.com>
2023-10-06 16:36:30 -0700

    Fix links

--- git-off.rb
+++ git-off.rb
@@ -5,7 +5,7 @@ version "0.0.1"
 
 on_macos do
     if Hardware::CPU.arm?
-    url "https://github.com/emorydunn/homebrew-git-off/releases/download/0.0.1/gif-off-arm64.tar.gz"
+    url "https://github.com/emorydunn/homebrew-git-off/releases/download/0.0.1/gif-off-arm64.zip"
     sha256 "6e264115843e6e9f33118a25d2134341c33b165397439adc6a8a59892ebc8b4f"
 
       def install
@@ -13,7 +13,7 @@ on_macos do
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/emorydunn/homebrew-git-off/releases/download/0.0.1/gif-off-amd64.tar.gz"
+      url "https://github.com/emorydunn/homebrew-git-off/releases/download/0.0.1/gif-off-amd64.zip"
       sha256 "23bf9be3cea8120c87efda617cb4f7ba314ea5fe40b7577ba9913784e56858a3"
 
       def install
