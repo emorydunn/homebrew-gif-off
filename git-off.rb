1028f29ac95169ec2ccedfa23b3efdcf324d8da7
Emory Dunn <emory@emorydunn.com>
2023-10-06 16:26:41 -0700

    Add Homebrew forumula

---
+++ git-off.rb
@@ -0,0 +1,31 @@
+class GitOff < Formula
+desc "Small CLI for making animated GIFs from folders of images."
+homepage "https://github.com/emorydunn/homebrew-git-off.git"
+version "0.0.1"
+
+on_macos do
+    if Hardware::CPU.arm?
+    url "https://github.com/emorydunn/homebrew-git-off/releases/download/0.0.1/gif-off-arm64.tar.gz"
+    sha256 "6e264115843e6e9f33118a25d2134341c33b165397439adc6a8a59892ebc8b4f"
+
+      def install
+        bin.install "git-off"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/emorydunn/homebrew-git-off/releases/download/0.0.1/gif-off-amd64.tar.gz"
+      sha256 "23bf9be3cea8120c87efda617cb4f7ba314ea5fe40b7577ba9913784e56858a3"
+
+      def install
+        bin.install "git-off"
+      end
+    end
+
+end
+
+def caveats
+    <<~EOS
+        See the Github repository for more information
+    EOS
+end
+end
