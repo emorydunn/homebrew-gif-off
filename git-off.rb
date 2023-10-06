8b63b941c7674ad30406a3c3936595aa29cb06d2
Emory Dunn <emory@emorydunn.com>
2023-10-06 16:45:01 -0700

    Fix binary name

--- git-off.rb
+++ git-off.rb
@@ -1,23 +1,23 @@
 class GitOff < Formula
 desc "Small CLI for making animated GIFs from folders of images."
-homepage "https://github.com/emorydunn/homebrew-git-off.git"
+homepage "https://github.com/emorydunn/homebrew-gif-off.git"
 version "0.0.1"
 
 on_macos do
     if Hardware::CPU.arm?
-    url "https://github.com/emorydunn/homebrew-git-off/releases/download/0.0.1/gif-off-arm64.zip"
+    url "https://github.com/emorydunn/homebrew-gif-off/releases/download/0.0.1/gif-off-arm64.zip"
     sha256 "6e264115843e6e9f33118a25d2134341c33b165397439adc6a8a59892ebc8b4f"
 
       def install
-        bin.install "git-off"
+        bin.install "gif-off"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/emorydunn/homebrew-git-off/releases/download/0.0.1/gif-off-amd64.zip"
+      url "https://github.com/emorydunn/homebrew-gif-off/releases/download/0.0.1/gif-off-amd64.zip"
       sha256 "23bf9be3cea8120c87efda617cb4f7ba314ea5fe40b7577ba9913784e56858a3"
 
       def install
-        bin.install "git-off"
+        bin.install "gif-off"
       end
     end
 
