b96c3d11075520d49106620412a274de9110f444
Emory Dunn <emory@emorydunn.com>
2023-10-21 18:42:17 -0700

    Used fixed version

--- gif-off.rb
+++ gif-off.rb
@@ -5,7 +5,7 @@ class GifOff < Formula
 
   on_macos do
       if Hardware::CPU.arm?
-      url "https://github.com/emorydunn/homebrew-gif-off/releases/download/#{version}/gif-off-arm64.zip"
+      url "https://github.com/emorydunn/homebrew-gif-off/releases/download/0.0.2/gif-off-arm64.zip"
       sha256 "fa96b7aadb17828022d1f8b2a77b6df300030571f242442d758355fbdd752292"
 
         def install
@@ -13,7 +13,7 @@ class GifOff < Formula
         end
       end
       if Hardware::CPU.intel?
-        url "https://github.com/emorydunn/homebrew-gif-off/releases/download/#{version}/gif-off-amd64.zip"
+        url "https://github.com/emorydunn/homebrew-gif-off/releases/download/0.0.2/gif-off-amd64.zip"
         sha256 "0b7443656abd8f61b5fbbe4a83adaee85adffe88e0b79fa42a260ee9d1f7698d"
 
         def install
