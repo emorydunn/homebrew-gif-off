1fe085fe704ded3093f41b0138070cd88e939743
Emory Dunn <emory@emorydunn.com>
2024-07-31 14:10:42 -0700

    Set version and update hashes

--- gif-off.rb
+++ gif-off.rb
@@ -1,12 +1,12 @@
 class GifOff < Formula
   desc "Small CLI for making animated GIFs from folders of images"
   homepage "https://github.com/emorydunn/homebrew-gif-off"
-  version "0.0.2"
+  version "0.1.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/emorydunn/homebrew-gif-off/releases/download/#{version}/gif-off-arm64.zip"
-      sha256 "82d2dad4fe545614ca2a15ca9fb618916420b36293632f61d34f30ff769c9d2b"
+      sha256 "8c97e03d55763281496403b8c7b88f97d2a4123ec6a88f7cbdfacc5b8b7891c7"
 
       def install
         bin.install "gif-off"
@@ -14,7 +14,7 @@ class GifOff < Formula
     end
     if Hardware::CPU.intel?
       url "https://github.com/emorydunn/homebrew-gif-off/releases/download/#{version}/gif-off-amd64.zip"
-      sha256 "d0565f5a3b68e1b6c7d8a32050dfd12e9e7987317269553738d7108a3de5cbe8"
+      sha256 "c5d39cfa77055e6a5189793f068dbe3dc40a41cf51c8de243f93bdd95548afc7"
 
       def install
         bin.install "gif-off"
